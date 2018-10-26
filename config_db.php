9c7f9a47f509ee3b6fe1184aa5e13151a000e8ae
roni.alvarenga <roni.alvarenga@campinas.sp.leg.br>
2018-10-26 13:55:54 -0300

    correção de arquivos

--- config_db.php
+++ config_db.php
@@ -1,7 +1,7 @@
 <?php
 class DB extends DBmysql {
    public $dbhost     = getenv('$MYSQL_ROOT_PASSWORD');
-   public $dbuser     = getenv('$MYSQL_DATABASE');
-   public $dbpassword = getenv('$MYSQL_USER');
-   public $dbdefault  = getenv('$MYSQL_PASSWORD');
+   public $dbuser     = getenv('$MYSQL_USER');
+   public $dbpassword = getenv('$MYSQL_PASSWORD');
+   public $dbdefault  = getenv('$MYSQL_DATABASE');
 }
